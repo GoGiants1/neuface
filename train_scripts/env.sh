89d16b1de4d2dbd99dadccfd4e51d366957f5bf3
GoGiants1 <chooi221@snu.ac.kr>
2022-06-13 18:03:40 +0900

    fix: modify env setup

--- train_scripts/env.sh
+++ train_scripts/env.sh
@@ -3,9 +3,9 @@ source ~/anaconda3/etc/profile.d/conda.sh
 conda activate dflab
 cd ..
 export DFL_PYTHON="python3"
-export DFL_WORKSPACE="/workspace/"
-export DFL_ROOT="/"
-export DFL_SRC="/deepface_lab"
+export DFL_WORKSPACE="workspace/"
+export DFL_ROOT="."
+export DFL_SRC="deepface_lab/"
 
 if [ ! -d "$DFL_WORKSPACE" ]; then
     mkdir "$DFL_WORKSPACE"
